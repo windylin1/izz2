26653a8c6f6f1c6a587b8f2335462d0de2b1504f
iris <jewel3@yeah.net>
2017-04-01 23:49:36 +0800

    update dao 1

--- entity/dao.js
+++ entity/dao.js
@@ -2,98 +2,147 @@ const db=require('../conf/db.js')
 const _ = require('underscore')._;
 const ustr = require('../lib/ustr.js');
 
-class dao{
-    async constructor(){
-        this.m = new Map(); //entities;
-        this.mcols = new Map();
-        this.cols = new Map();
+let m = new Map(); //entities;
+let mcols = new Map();
+let cols = new Map();
 
+if(true){
+   await init();
+}
+
+function async init(){
         //get m from db; 这里不应该是异步的; 暂时先这么处理;
-        let ls =await db.query('select * from BASE_ENTITY',null);
+    let ls =await db.query('select * from BASE_ENTITY',null);
 
-        if(!ls){
-            throw new Error('未定义任何实体!'); //这里应该退出程序;
+    if(!ls){
+        throw new Error('未定义任何实体!'); //这里应该退出程序;
+    }
+
+    for(let a of ls){
+        m.set(a.EntityCode,a);
+        mcols.set(a.EntityCode,null);
+    }
+
+    let ls2 = await db.query('select * from BASE_ENTITY_ATTR',null);
+
+    if(!ls2){
+        throw new Error('未定义任何实体属性');
+    }
+
+    for(let c of ls2){
+        cols.set(c.Fid,c);
+
+        if(!m.has(c.EntityCode)){ continue;}
+
+        let arr = new Set();
+        if(mcols.get(c.EntityCode)){
+            arr = mcols.get(c.EntityCode);
         }
 
-        for(let a of ls){
-            this.m.set(a.EntityCode,a);
-            this.mcols.set(a.EntityCode,null);
-        }
+        arr.add(c.Fid);
+        mcols.set(c.EntityCode,arr);
+    }
 
-        let ls2 = await db.query('select * from BASE_ENTITY_ATTR',null);
+    //递归父级对象的属性; 如果子级已经有的,以子级为准;
+    let _tmp = _.pluck(_.filter(m.values(),function(a){return !ustr.isnul(a.EntityPCode);}),'EntityPCode');
+    let _prts = new Set(_tmp);
+    if(_prts){
+       _prts = new Set(_.pluck(_.filter(m.values(),function(a){return _prts.has(a.EntityCode)&&ustr.isnul(a.EntityPCode);}),'EntityCode'));
+    }
 
-        if(!ls2){
-            throw new Error('未定义任何实体属性');
-        }
+    let _prtsNew = new Set();
 
-        for(let c of ls2){
-            this.cols.set(c.Fid,c);
+    while(_prts){
+        for(let k of m.keys()){
+            
+            if( _prts.has(m.get(k).EntityPCode)){
 
-            if(!this.m.has(c.EntityCode)){ continue;}
+                let arr = mcols.get(k);
+                let parr = mcols.get(m.get(k).EntityPCode);
 
-            let arr = new Set();
-            if(this.mcols.get(c.EntityCode)){
-                arr = this.mcols.get(c.EntityCode);
-            }
+                let arrls = getlsByKey(arr,cols);
+                let parrls = getlsByKey(parr,cols);
 
-            arr.add(c.Fid);
-            this.mcols.set(c.EntityCode,arr);
-        }
+                let arrNames =new Set(_.pluck(arrls,'FldName'));
 
-        //递归父级对象的属性; 如果子级已经有的,以子级为准;
-        let _tmp = _.pluck(_.filter(this.m.values(),function(a){return !ustr.isnul(a.EntityPCode);}),'EntityPCode');
-        let _prts = new Set(_tmp);
-        if(_prts){
-           _prts = new Set(_.pluck(_.filter(this.m.values(),function(a){return _prts.has(a.EntityCode)&&ustr.isnul(a.EntityPCode);}),'EntityCode'));
-        }
+                let newCols =_.pluck(_.filter(parrls,function(a){return !arrNames.has(a.FldName);}),'Fid');
 
-        let _prtsNew = new Set();
-
-        while(_prts){
-            for(let k of this.m.keys()){
-                
-                if( _prts.has(this.m.get(k).EntityPCode)){
-
-                    let arr = this.mcols.get(k);
-                    let parr = this.mcols.get(this.m.get(k).EntityPCode);
-
-                    let arrls = dao.getlsByKey(arr,this.cols);
-                    let parrls = dao.getlsByKey(parr,this.cols);
-
-                    let arrNames =new Set(_.pluck(arrls,'FldName'));
-
-                    let newCols =_.pluck(_.filter(parrls,function(a){return !arrNames.has(a.FldName);}),'Fid');
-
-                    for(let a of newCols){
-                        arr.add(a);
-                    }
-                    this.mcols.set(k,arr);
-
-                    _prtsNew.add(k);
+                for(let a of newCols){
+                    arr.add(a);
                 }
-            }
-            _prts = _prtsNew;
-        }
+                mcols.set(k,arr);
 
-        //finish
-
-    }
-    
-    function static getlsByKey(keyls,m1){
-        let ls = new Array();
-        for(let k of keyls){
-            if(m1.has(k)){
-                ls1.push(m1.get(k));
+                _prtsNew.add(k);
             }
         }
-        return ls;
+        _prts = _prtsNew;
     }
 
+    //finish
+}
 
-    //获取某实体;
-    //获取某些实体;
-    
+//获取某实体;
+function async first(sa,entityCode,flds="*",sqlWhere="",params=null){
+
+    let ls = await findBySql(sa,entityCode,flds,sqlWhere,params);
+    if(ls){
+        return ls[0];
+    }
+    return null;
+}
+
+function async firstByKey(sa,entityCode,key,flds="*"){
 
 }
+
+function async find(sa,entityCode,flds="*",sqlWhere="",params=null,sqlOrder="",ispage=0,ipage=0,isize=10){
+    
+    if(!m.has(entityCode)) return null;
+
+    let tmp = m.get(entityCode);
+
+    let selsql = "select "+flds+ " from "+tmp.TblName;
+
+    let ls = await findBySql(sa,entityCode,selsql,sqlWhere,params,sqlOrder,ispage,ipage,isize);
+
+    return ls;
+}
+
+//获取某些实体;
+function async findBySql(sa,entityCode,selsql,sqlWhere="",params=null,sqlOrder="",ispage=0,ipage=0,isize=10){
+
+    if(!(sa.isSYS||sa.isAdmin)){
+        sqlWhere = _addUaCtl(sa,entityCode,sqlWhere);
+    }
+
+}    
+
+//更新
+function async update(){
+
+}
+
+//增加
+function async add(){
+
+}
+
+
+function _addUaCtl(sa,entityCode,sqlWhere){
+
+}
+
+
+
+function getlsByKey(keyls,m1){
+    let ls = new Array();
+    for(let k of keyls){
+        if(m1.has(k)){
+            ls1.push(m1.get(k));
+        }
+    }
+    return ls;
+}
+
 
 
